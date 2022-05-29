9d1d3f06a0d4016daec85ae7be07736ab2c86017
unknown <gustavo.lach@gmail.com>
2022-05-29 12:08:10 -0300

    add change color to ColorHolder

--- src/main/java/graphical/basics/ColorHolder.java
+++ src/main/java/graphical/basics/ColorHolder.java
@@ -1,6 +1,10 @@
 package graphical.basics;
 
 
+import graphical.basics.presentation.Presentation;
+import graphical.basics.task.Task;
+import graphical.basics.task.transformation.gobject.ColorListTranform;
+
 import java.awt.*;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -38,5 +42,13 @@ public class ColorHolder {
 
     public ColorHolder copy() {
         return new ColorHolder(color);
+    }
+
+    public Task changeColor(Color c) {
+        return changeColor(c, Presentation.staticReference.seconds(1));
+    }
+
+    public Task changeColor(Color color, int steps) {
+        return new ColorListTranform(List.of(this),List.of(color),steps);
     }
 }
